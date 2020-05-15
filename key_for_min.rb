9427b6b057c952473079731c2f726cdfcd669bcf
William Rogge <williamroggeiv@gmail.com>
2020-05-15 18:11:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
   min = nil  
   name = nil
   hash.each do |person, num|
-    if num==min || num < min
+    if num < min || num==min
       min = num
       name = person
     end
