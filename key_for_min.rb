bd386e0fba1e800f49c7adf479bc4b1d0e5aa97e
William Rogge <williamroggeiv@gmail.com>
2020-05-15 18:07:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  
-if
   min = nil  
   name = nil
   hash.each do |person, num|
@@ -11,7 +9,6 @@ if
       min = num
       name = person
     end
-end
 name
 end
 end
