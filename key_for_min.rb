e4a3d216b16578c3df35df083995975236af9980
William Rogge <williamroggeiv@gmail.com>
2020-05-15 16:21:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(my_hash)
-if my_hash {}
+if my_hash = {}
 return "nil"
 else
  min = my_hash.first[1]
